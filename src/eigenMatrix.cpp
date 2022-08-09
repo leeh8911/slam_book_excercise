ee1fc1b0d7ae3afd5d9582d02173174e534995e5
sangwon <leeh8911@gmail.com>
2022-08-10 05:51:25 +0900

    update ch3

--- src/eigenMatrix.cpp
+++ src/eigenMatrix.cpp
@@ -10,5 +10,12 @@
 
 int Chapter3(void) {
     Eigen::Matrix<float, 2, 3> matrix_23;
+    std::cout << "Create 2x3 Matrix" << std::endl;
     std::cout << matrix_23 << std::endl;
+
+    Eigen::Vector3d v_3d;
+    std::cout << "Create 3d vector" << std::endl;
+    std::cout << v_3d << std::endl;
+
+    return 0;
 }
